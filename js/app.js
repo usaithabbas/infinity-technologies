72ef620cf1e8022dc33e5007df7c6bbef6dc7f02
Mohammed Usaith Abbas.S <pcasd21cs029@gmail.com>
2023-07-20 05:22:28 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -112,3 +112,16 @@ var typed = new Typed(".typing", {
     backSpeed: 60,
     loop: true
 });
+// <!--Start of Tawk.to Script-->
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/647c323c7957702c744ba635/1h22i8g9h';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
+// <!--End of Tawk.to Script-->
